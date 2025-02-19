08e4c0e690952d1531f4c9d7a74293c422580309
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-02-20 01:54:42 +0530

    Add Page routes for about, articles, articles/individuals

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,10 +4,22 @@ import {
 } from "react-router-dom";
 import './App.css'
 import HomePage from "./pages/HomePage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import ArticleList from "./pages/ArticleList.jsx";
+import ArticlePage from "./pages/ArticlePage.jsx";
 
 const routes = [{
     path: '/',
     element: <HomePage />,
+}, {
+    path: '/about',
+    element: <AboutPage />,
+}, {
+    path: '/articles',
+    element: <ArticleList />,
+}, {
+    path: '/articles/individuals',
+    element: <ArticlePage />,
 }];
 
 const router = createBrowserRouter(routes);
