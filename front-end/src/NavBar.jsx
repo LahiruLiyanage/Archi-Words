537486b9af599313dfb37a2a32049667ba4d9378
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-02-21 20:33:13 +0530

    Add Some Styling to the Pages

--- front-end/src/NavBar.jsx
+++ front-end/src/NavBar.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
     return (
         <nav>
+            <div className="logo">
+                Archi Words
+            </div>
             <ul>
                 <li>
                     <Link to='/'>Home</Link>
