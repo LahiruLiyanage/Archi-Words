7fc48867f9706107aaaa665b9d41ae110a22b523
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-02-22 16:29:06 +0530

    Define add End point to the backend server and testing

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,9 +2,15 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/hello', function(req, res) {
-    res.send('Hello!');
+    res.send('Hello from GET!');
 });
+
+app.post('/hello', function(req, res) {
+    res.send('Hello, ' + req.body.name + ' from POST!');
+})
 
 app.listen(8000, function() {
     console.log('Express server is listening on port 8000');
