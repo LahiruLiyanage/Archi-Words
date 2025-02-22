baf5d55068990c8f4fb875bfc379dd64da754b5b
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-02-22 21:42:31 +0530

    Setup article comments and getting the respond through json

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
 const articleInfo = [
-    { name: 'sd', upvotes: 0 },
-    { name: 'mm', upvotes: 0 },
-    { name: 'up', upvotes: 0 },
+    { name: 'sd', upvotes: 0, comments: [] },
+    { name: 'mm', upvotes: 0, comments: [] },
+    { name: 'up', upvotes: 0, comments: [] },
 ]
 
 const app = express();
@@ -14,7 +14,21 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     const article = articleInfo.find(article => article.name === req.params.name);
     article.upvotes += 1;
 
-    res.send('Success! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes.');
+    res.json(article);
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articleInfo.find(article => article.name === name);
+
+    article.comments.push({
+        postedBy,
+        text,
+    });
+
+    res.json(article);
 })
 
 app.listen(8000, function() {
