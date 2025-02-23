6565a72f7011f8f1a91a51df93b876c0314a0ff8
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-02-23 21:05:26 +0530

    Add Login Button SetUp the logic using auth"
    q

--- front-end/src/NavBar.jsx
+++ front-end/src/NavBar.jsx
@@ -1,6 +1,14 @@
 import { Link } from "react-router-dom";
+import {useState} from "react";
+import { useNavigate } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
 
 export default function NavBar() {
+    const isLoggedIn = false;
+    // const email = 'lahaliya@gmail.com';
+
+    const navigate = useNavigate();
+
     return (
         <nav>
             <div className="logo">
@@ -15,6 +23,19 @@ export default function NavBar() {
                 </li>
                 <li>
                     <Link to='/articles'>Articles</Link>
+                </li>
+                {isLoggedIn && (
+                    <li style={{ color: "white" }}>
+                        Logged in as {email}
+                    </li>
+                )}
+                <li>
+                    {isLoggedIn ? (
+                        <button onClick={() => signOut(getAuth())}>Sign Out</button>
+                    ) : (
+                        <button onClick={() => navigate('/login')}>Log In</button>
+                    )}
+
                 </li>
             </ul>
         </nav>
